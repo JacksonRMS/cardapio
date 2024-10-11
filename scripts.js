d79f18b21c2834ade2d9c05f478381ab676a5eb3
JacksonRMS <jacktjrubem@gmail.com>
2024-10-11 14:05:19 -0300

    Dando andamento no projeto

--- scripts.js
+++ scripts.js
@@ -1,10 +1,42 @@
-const card = document.querySelector('#card')
+const list = document.querySelector('ul') //lista não ordenada
+const cards = document.querySelector('#cards') //primeiro botão = ForEach - showALL
+const botaoDeDesconto = document.querySelector('#desconto') //map-all
 
-let myLyst = ''
+cards.addEventListener('click', () => mostrarTudo (menuOptions) )//showAll
+botaoDeDesconto.addEventListener('click', descont)
 
-menuOptions.forEach( produto =>{
+
+function mostrarTudo(productsArray) { //showAll
+    let myLyst = ''
     
-})
+    productsArray.forEach((produto) => {
+        myLyst += `
+                <li>
+                    <img src = ${produto.src}>
+                    <p>${produto.name}</p>
+                    <p>${produto.price}</p>
+                </li>
+        `
+    })
+    list.innerHTML = myLyst
+}
+
+function descont() { //mapALLItems
+    const novosPrecos /*newPrices*/ = menuOptions.map((produto) => ({
+        ...produto, //Spread Operator
+        price: produto.price * 0.9,
+    }))
+
+   mostrarTudo(novosPrecos) //showALL
+
+}
 
 
-console.log( card)
+
+
+
+
+
+
+
+
